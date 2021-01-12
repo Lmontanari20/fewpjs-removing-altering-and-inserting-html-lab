3fd16661ab02e1dd416bf44e6741eaa7bbfe8d57
Lucas Montanari <Lmonty2011@gmail.com>
2021-01-11 19:11:27 -0800

    commit tree e667b99cc63507bbd522244e6b30669b1a67a5d0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let main = document.querySelector('main#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.innerHTML("H1");
+document.body.appendChild(newHeader);
+newHeader.className("victory");
+
+newHeader.innerHTML += "victory";
+newHeader.innerHTML += "is the champion";
